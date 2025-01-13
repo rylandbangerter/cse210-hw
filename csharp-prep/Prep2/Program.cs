ab9b43ff8c2842d7343a3b3ae6edf56d14b8927b
Ryland Bangerter <rylandbangerter06@gmail.com>
2025-01-13 10:44:52 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+
+        string letter = "";
+
+        if(number >= 90) {
+            letter = "A";
+        }
+        else if (number >= 80) {
+            letter = "B";
+        }
+        else if (number >= 70) {
+            letter = "C";        
+        }
+        else if (number >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if(number >= 70){
+            Console.WriteLine("Congratulations! You passed!");
+        } else {
+            Console.WriteLine("You got it next time!");
+        }
     }
 }
